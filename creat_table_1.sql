7ab9f90bffcc7b3243a6cbaaa66ff6fa38e0224f
RiverJAM <thetutorjoemonahan@gmail.com>
2020-11-21 11:33:39 -0600

    adds a bunch of stuff

---
+++ creat_table_1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE employees (
+    emp_no INT   NOT NULL,
+    emp_title_id VARCHAR(255)   NOT NULL,
+    birth_date date   NOT NULL,
+    first_name VARCHAR(255)   NOT NULL,
+    last_name VARCHAR(255)   NOT NULL,
+    sex VARCHAR(255)   NOT NULL,
+    hire_date DATE   NOT NULL,
+     PRIMARY KEY (emp_no)
+
+);
